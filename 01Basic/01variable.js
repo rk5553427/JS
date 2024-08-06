08cf55345512d069c8744897057f3ee4caa3b8a1
Ravinder Kumar <rk5553427@gmail.com>
2024-08-06 16:58:46 +0000

    variable done

---
+++ 01Basic/01variable.js
@@ -0,0 +1,14 @@
+const accountId = 10001
+let accountEmail ="ravinder46256@gmail"
+var accountPassword="123456" //Not Use
+accountCity="SNR" //Not use
+
+//accountId=2 Not Allowed
+accountEmail="rk44@gmail.com"
+accountPassword="32154"
+accountCity="MANDI"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
+
+/*perfer not to use var 
+because of issue in block scope and functional scope*/
