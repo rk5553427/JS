c8190060eb1a5a1c9f5116732a2bb252b43e97e6
Ravinder Kumar <rk5553427@gmail.com>
2024-08-07 16:01:36 +0000

    datatype

---
+++ 01datatypes/01datatype.js
@@ -0,0 +1,22 @@
+"user strict" //treat all JS code as newer version
+//alert('Hello') // we are using nodejs not browser
+
+let name="raivinder"
+let age=10
+let isloggedin=false
+
+// Number;
+// String;
+// BigInt;
+// Boolean;
+// null; it is object
+// undefined;
+// Symbol;
+
+// Object;
+// console.log(typeof undefined); //object
+// console.log(typeof null); //object
+
+
+
+https://tc39.es/ecma262/#sec-numbers-and-dates
